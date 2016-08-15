083b457aa8d330da283d86e078bd77dff6bcdbd3
Xavier Boubert <xavier@boubert.fr>
2016-08-16 00:36:22 +0200

    feat(async): create feature

---
+++ features/async/async-gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = ['$gulp', function($gulp) {
+
+  var sourcemaps = require('gulp-sourcemaps'),
+      uglify = require('gulp-uglify'),
+      rename = require('gulp-rename');
+
+  $gulp.task('async', function(done) {
+    $gulp
+      .src('node_modules/async/dist/async.js')
+      .pipe($gulp.dest('./public/vendor'))
+      .pipe(sourcemaps.init())
+      .pipe(uglify())
+      .pipe(rename({
+        extname: '.min.js'
+      }))
+      .pipe(sourcemaps.write('./'))
+      .pipe($gulp.dest('./public/vendor'))
+      .on('end', done);
+  });
+
+  return 'async';
+}];
